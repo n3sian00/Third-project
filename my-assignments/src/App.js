2e70c6e0714c36fc9a5ae816cf06c2cda7504dd4
n3sian00 <n3sian00@students.oamk.fi>
2024-11-11 16:55:01 +0200

    Added MainUI and ChildComponent structure

--- my-assignments/src/App.js
+++ my-assignments/src/App.js
@@ -1,28 +1,17 @@
-import React, { useState } from 'react';
-import Header from './Header';
-import ProductForm from './ProductForm';
-import OrderInfo from './OrderInfo';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainUI from './components/MainUI';
+import AnotherComponent from './components/AnotherComponent';
 
 function App() {
-    const products = [
-        { name: "Product A", price: 10.0 },
-        { name: "Product B", price: 15.0 },
-        { name: "Product C", price: 20.0 },
-    ];
-
-    const [selectedOrder, setSelectedOrder] = useState({ product: products[0], quantity: 1 });
-
-    const handleOrderChange = (product, quantity) => {
-        setSelectedOrder({ product, quantity });
-    };
-
-    return (
-        <div>
-            <Header title="Welcome to Product Page!" />
-            <ProductForm products={products} onOrderChange={handleOrderChange} />
-            <OrderInfo product={selectedOrder.product} quantity={selectedOrder.quantity} />
-        </div>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainUI />} />
+        <Route path="/another" element={<AnotherComponent />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ my-assignments/src/components/MainUI.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChildComponent from './ChildComponent';
+
+function MainUI() {
+  return (
+    <div>
+      <h1>Main UI Component</h1>
+      <ChildComponent />
+    </div>
+  );
+}
+
+export default MainUI;
